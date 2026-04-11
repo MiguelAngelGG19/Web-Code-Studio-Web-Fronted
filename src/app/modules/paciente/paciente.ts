04f9235a565afc31e57472e1184a428e6699d412
MiguelAngelGG19 <a3524110645@alumno.uttehuacan.edu.mx>
2026-04-11 17:09:29 -0600

    fix: add standalone:true to Paciente component

--- src/app/modules/paciente/paciente.ts
+++ src/app/modules/paciente/paciente.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-paciente',
+  standalone: true,
   imports: [],
   templateUrl: './paciente.html',
   styleUrl: './paciente.scss',
