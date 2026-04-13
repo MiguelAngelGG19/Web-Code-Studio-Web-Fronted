15207abc374688778090bf52a45a1a59ada8b0d7
MiguelAngelGG19 <a3524110645@alumno.uttehuacan.edu.mx>
2026-04-13 05:23:16 -0600

    fix(env): set production backend URL to Railway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   version: 'prod.0.0.1',
   webservice: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://impartial-art-production.up.railway.app',
   }
 };
